c653b70800e767130d3e0e85783e8acfcf77138b
TartCodes <tartcodes@gmail.com>
2023-02-14 07:36:08 -0500

    fetch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const [movies, setMovies] = useState([]);
+	const fetchMoviesHandlder = () => {
+		fetch("https://swapi.py4e.com/api/films/")
+			.then((res) => res.json())
+			.then((data) => setMovies(data.results));
+	};
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<section>
+				<button>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={movies} />
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
