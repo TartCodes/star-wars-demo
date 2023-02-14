3da7218165279af29ad93efcba1dfaa3bd2486a7
TartCodes <tartcodes@gmail.com>
2023-02-14 07:45:56 -0500

    loading state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import "./App.css";
 
 function App() {
 	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
 	const fetchMoviesHandler = async () => {
+		setIsLoading(true);
 		const res = await fetch("https://swapi.py4e.com/api/films/");
 		const data = await res.json();
 
@@ -19,6 +22,7 @@ function App() {
 			};
 		});
 		setMovies(transformedMovies);
+		setIsLoading(false);
 	};
 
 	return (
@@ -27,7 +31,8 @@ function App() {
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
 			<section>
-				<MoviesList movies={movies} />
+				{!isLoading && <MoviesList movies={movies} />}
+				{isLoading && <p>Loading...</p>}
 			</section>
 		</React.Fragment>
 	);
